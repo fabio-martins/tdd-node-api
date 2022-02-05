62424babc331bd6730a38246f39197df57514d04
Fabio <martins.fabiojr@gmail.com>
2022-02-04 22:35:00 -0300

    corrected throws exceptions

--- src/domain/usecases/auth-usecases.spec.js
+++ src/domain/usecases/auth-usecases.spec.js
@@ -3,10 +3,10 @@ const { MissingParamError } = require('../../utils/errors')
 class AuthUseCase {
     async auth(email, password) {
         if (!email) {
-            return new MissingParamError('email')
+            throw new MissingParamError('email')
         }
         if (!password) {
-            return new MissingParamError('password')
+            throw new MissingParamError('password')
         }
     }
 }
@@ -17,12 +17,16 @@ describe('Auth Usecase', () => {
         const promisse = sut.auth()
         expect(promisse).rejects.toThrow(new MissingParamError('email'))
     })
-})
 
-describe('Auth Usecase', () => {
     test('Should throw if no password is provided', async () => {
         const sut = new AuthUseCase()
         const promisse = sut.auth('any_email@mail.com')
         expect(promisse).rejects.toThrow(new MissingParamError('password'))
     })
-})
+
+    test('Should throw if no password is provided', async () => {
+        const sut = new AuthUseCase()
+        const promisse = sut.auth('any_email@mail.com')
+        expect(promisse).rejects.toThrow(new MissingParamError('password'))
+    })
+})    
