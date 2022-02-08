6ab5d9137075ac3deceedac751744c66927edff7
Fabio <martins.fabiojr@gmail.com>
2022-02-07 22:59:31 -0300

    ensure AuthUseCasa throws if no dependencies are provided

--- src/domain/usecases/auth-usecases.spec.js
+++ src/domain/usecases/auth-usecases.spec.js
@@ -78,7 +78,7 @@ describe('Auth Usecase', () => {
     })
 
     test('Should throw if no LoadUserByEmailRepository is provide ', async () => {
-        const sut = new AuthUseCase({})
+        const sut = new AuthUseCase()
         const promise = sut.auth('any_email@mail.com', 'any_password')
         expect(promise).rejects.toThrow(new MissingParamError('loadUserByEmailRepository'))
     })
