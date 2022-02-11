5bcb16ec511386d03e55e6c248e259e15c15ce06
Fabio <martins.fabiojr@gmail.com>
2022-02-10 22:57:37 -0300

    ensure TokenGenerator throws if no secret is provided

--- src/presentation/helpers/token-generator.spec.js
+++ src/presentation/helpers/token-generator.spec.js
@@ -1,11 +1,15 @@
 const jwt = require('jsonwebtoken')
+const MissingParamError = require('../../utils/errors/missing-param-error')
 
 class TokenGenerator {
     constructor(secret) {
-        this.secret
+        this.secret = secret
     }
 
     async generate(id) {
+        if (!this.secret) {
+            throw new MissingParamError('secret')
+        }
         return jwt.sign(id, this.secret)
     }
 }
@@ -31,5 +35,10 @@ describe('Token Generator', () => {
         await sut.generate('any_id')
         expect(jwt.id).toBe('any_id')
         expect(jwt.secret).toBe(sut.secret)
+    })
+    test('should throw if no secret is provided', async () => {
+        const sut = new TokenGenerator()
+        const promise = sut.generate('any_id')
+        expect(promise).rejects.toThrow(new MissingParamError('secret'))
     })
 })
